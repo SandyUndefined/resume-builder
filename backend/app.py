c3665f884aa7e13b50d01a7bfb812a5324f1812b
Sandy_undefined <08sandysk@gmail.com>
2021-06-07 21:44:02 +0530

    basic setup

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+
+app = Flask(__name__)
+bot = ChatBot("Sunanda's Resume ChatBot")
+trainer = ListTrainer(bot)
+
+file =  open("static/file.text", "r")
+conversation = file.read()
+
+trainer.train(conversation)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/")
+def get_bot_response():
+    userText = request.args.get('msg')
+    print("working")
+    return str(bot.get_response(userText))
+
+if __name__ == "__main__":
+    app.run()
